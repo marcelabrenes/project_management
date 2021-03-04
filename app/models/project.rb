01fb320fa97a22bce0edeeba32fad5cf2e53029d
marcelabrenes <marce89455@gmail.com>
2021-03-04 16:58:58 -0600

    Project done

---
+++ app/models/project.rb
@@ -0,0 +1,6 @@
+class Project < ApplicationRecord
+    validates :name, presence: true
+    validates :description, presence: true
+    validates :status, presence: true
+    enum status: [:proposal, :progress, :finished]
+end
